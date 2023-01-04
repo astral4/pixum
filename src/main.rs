1fb5b97fa28eb02820fb2d6673edec092719ae10
astral4 <88992929+astral4@users.noreply.github.com>
2023-01-04 01:23:04 -0500

    add tower middleware

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![warn(clippy::all, clippy::pedantic)]
 #![forbid(unsafe_code)]
 
-use axum::{http::StatusCode, routing::get, Router, Server};
+use axum::{error_handling::HandleErrorLayer, http::StatusCode, routing::get, Router, Server};
 use pixum::{work, AppState};
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
+use tower::ServiceBuilder;
 
 #[allow(clippy::unused_async)]
 async fn fallback() -> (StatusCode, String) {
@@ -18,6 +19,7 @@ async fn main() {
     let shared_state = Arc::new(AppState::new());
 
     let app = Router::new()
+        .fallback(fallback)
         .route("/", get(|| async { "Welcome to Pixum" }))
         .route(
             "/:work_id",
@@ -28,7 +30,19 @@ async fn main() {
             get(work::source).with_state(shared_state.clone()),
         )
         .with_state(shared_state)
-        .fallback(fallback);
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|_| async {
+                    StatusCode::TOO_MANY_REQUESTS
+                }))
+                .buffer(20)
+                .rate_limit(5, Duration::from_secs(1))
+                .layer(HandleErrorLayer::new(|_| async {
+                    StatusCode::REQUEST_TIMEOUT
+                }))
+                .timeout(Duration::from_secs(10))
+                .concurrency_limit(100),
+        );
 
     Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
